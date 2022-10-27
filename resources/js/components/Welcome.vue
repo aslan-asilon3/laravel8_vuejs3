3a8c1cef8f0aed618f367a7596e8ce65efc4593e
aslan-asilon3 <sulaslansetiawan3@gmail.com>
2022-10-26 18:46:22 -0700

    first commit

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Welcome Page</h2>
+</template>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Vuejs</title>
+</head>
+<body class="antialiased">
+    <div id="app">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center py-4 sm:pt-0">
+            <welcome/>
+        </div>
+    </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
